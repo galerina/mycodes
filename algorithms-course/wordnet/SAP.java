fe125ee9c3ee71836af28cc54fdcc95a4625576b
Andrew Spencer <andrewpspencer@gmail.com>
2015-11-12 23:44:46 +0900

    Wordnet project checkpoint #2: pre-search refactoring

--- algorithms-course/wordnet/SAP.java
+++ algorithms-course/wordnet/SAP.java
@@ -6,159 +6,184 @@ import edu.princeton.cs.algs4.Queue;
 
 import java.util.Arrays;
 
+// Quoted from http://coursera.cs.princeton.edu/algs4/assignments/wordnet.html:
+//
+//   ...An ancestral path between two vertices v and w in a digraph is a directed path
+//   from v to a common ancestor x, together with a directed path from w to the same
+//   ancestor x. A shortest ancestral path is an ancestral path of minimum total length...
+//
+// The SAP class is initialized with a directed graph and it allows a user to query  
+// information about the the shortest ancestral path for any two nodes in that graph.  
+// The methods are length(), which returns the length of the SAP, and ancestor(), which
+// returns the ancestor vertex.
 public class SAP {
-    Digraph digraph;
-    int sapLength;
-    int sapAncestor;
+    private Digraph digraph;
+    private int sapLength;
+    private int sapAncestor;
 
     // constructor takes a digraph (not necessarily a DAG)
     public SAP(Digraph G) {
-	if (G == null) {
-	    throw new java.lang.NullPointerException();
-	}
-	digraph = new Digraph(G);
+        if (G == null) {
+            throw new java.lang.NullPointerException();
+        }
+        digraph = new Digraph(G);
     }
 
-   // length of shortest ancestral path between v and w; -1 if no such path
-   public int length(int v, int w) {
-       calculateSAP(v,w);
-       return sapLength;
-   }
+    // length of shortest ancestral path between v and w; -1 if no such path
+    public int length(int v, int w) {
+	if (!isValidVertex(digraph, v) || !isValidVertex(digraph, w)) {
+	    throw new IndexOutOfBoundsException();
+	}
+	calculateSAP(v,w);
+	return sapLength;
+    }
 
-   // a common ancestor of v and w that participates in a shortest ancestral path; -1 if no such path
-   public int ancestor(int v, int w) {
-       calculateSAP(v,w);
-       return sapAncestor;
-   }
+    // a common ancestor of v and w that participates in a shortest ancestral path; -1 if no such path
+    public int ancestor(int v, int w) {
+	if (!isValidVertex(digraph, v) || !isValidVertex(digraph, w)) {
+	    throw new IndexOutOfBoundsException();
+	}
+	calculateSAP(v,w);
+	return sapAncestor;
+    }
 
-   // length of shortest ancestral path between any vertex in v and any vertex in w; -1 if no such path
-   public int length(Iterable<Integer> v, Iterable<Integer> w) {
-       if (v == null || w == null) {
-	   throw new NullPointerException();
-       }
+    // length of shortest ancestral path between any vertex in v and any vertex in w; -1 if no such path
+    public int length(Iterable<Integer> v, Iterable<Integer> w) {
+	if (v == null || w == null) {
+	    throw new NullPointerException();
+	}
 
-       calculateSAP(v, w);
-       return sapLength;
-   }
+	calculateSAP(v, w);
+	return sapLength;
+    }
 
-   // a common ancestor that participates in shortest ancestral path; -1 if no such path
-   public int ancestor(Iterable<Integer> v, Iterable<Integer> w) {
-       if (v == null || w == null) {
-	   throw new NullPointerException();
-       }
+    // a common ancestor that participates in shortest ancestral path; -1 if no such path
+    public int ancestor(Iterable<Integer> v, Iterable<Integer> w) {
+	if (v == null || w == null) {
+	    throw new NullPointerException();
+	}
 
-       calculateSAP(v, w);
-       return sapAncestor;
-   }
+	calculateSAP(v, w);
+	return sapAncestor;
+    }
 
-   private void calculateSAP(int v, int w) {
-       if (v == w) {
-	   sapAncestor = v;
-	   sapLength = 0;
-	   return;
-       }
+    private void calculateSAP(int v, int w) {
+	if (v == w) {
+	    sapAncestor = v;
+	    sapLength = 0;
+	    return;
+	}
 
-       // Do a concurrent breadth first search starting from each node v and w
-       // which terminates when we reach a vertex that has already been marked 
-       // by the other node. Maintain separate marked and distTo arrays for
-       // each vertex
-       Queue<Integer> vq = new Queue<Integer>();
-       Queue<Integer> wq = new Queue<Integer>();
+	// Do a concurrent breadth first search starting from each node v and w
+	// which terminates when we reach a vertex that has already been marked 
+	// by the other node. Maintain separate marked and distTo arrays for
+	// each vertex
+	Queue<Integer> vq = new Queue<Integer>();
+	Queue<Integer> wq = new Queue<Integer>();
 
-       boolean[] vMarked = new boolean[digraph.V()];
-       boolean[] wMarked = new boolean[digraph.V()];
-       int[] vDistTo = new int[digraph.V()];
-       int[] wDistTo = new int[digraph.V()];
+	boolean[] vMarked = new boolean[digraph.V()];
+	boolean[] wMarked = new boolean[digraph.V()];
+	int[] vDistTo = new int[digraph.V()];
+	int[] wDistTo = new int[digraph.V()];
 
-       vMarked[v] = true;
-       vDistTo[v] = 0;
-       vq.enqueue(v);
+	vMarked[v] = true;
+	vDistTo[v] = 0;
+	vq.enqueue(v);
 
-       wDistTo[w] = 0;
-       wMarked[w] = true;
-       wq.enqueue(w);
+	wMarked[w] = true;
+	wDistTo[w] = 0;
+	wq.enqueue(w);
 
-       while (!vq.isEmpty() || !wq.isEmpty()) {
-	   if (!vq.isEmpty()) {
-	       // v search
-	       int current = vq.dequeue();
-	       if (wMarked[current]) {
-		   sapLength = vDistTo[current] + wDistTo[current];
-		   sapAncestor = current;
-		   return;
-	       }
-	       for (int next : digraph.adj(current)) {
-		   if (!vMarked[next]) {
-		       vDistTo[next] = vDistTo[current] + 1;
-		       vMarked[next] = true;
-		       vq.enqueue(next);
-		   }
-	       }
-	   }
+	while (!vq.isEmpty() || !wq.isEmpty()) {
+	    if (!vq.isEmpty()) {
+		// v search
+		int current = vq.dequeue();
+		if (wMarked[current]) {
+		    sapLength = vDistTo[current] + wDistTo[current];
+		    sapAncestor = current;
+		    return;
+		}
+		for (int next : digraph.adj(current)) {
+		    if (!vMarked[next]) {
+			vDistTo[next] = vDistTo[current] + 1;
+			vMarked[next] = true;
+			vq.enqueue(next);
+		    }
+		}
+	    }
 
-	   if (!wq.isEmpty()) {
-	       // w search
-	       int current = wq.dequeue();
-	       if (vMarked[current]) {
-		   sapLength = vDistTo[current] + wDistTo[current];
-		   sapAncestor = current;
-		   return;
-	       }
-	       for (int next : digraph.adj(current)) {
-		   if (!wMarked[next]) {
-		       wDistTo[next] = wDistTo[current] + 1;
-		       wMarked[next] = true;
-		       wq.enqueue(next);
-		   }
-	       }
-	   }
-       }
+	    if (!wq.isEmpty()) {
+		// w search
+		int current = wq.dequeue();
+		if (vMarked[current]) {
+		    sapLength = vDistTo[current] + wDistTo[current];
+		    sapAncestor = current;
+		    return;
+		}
+		for (int next : digraph.adj(current)) {
+		    if (!wMarked[next]) {
+			wDistTo[next] = wDistTo[current] + 1;
+			wMarked[next] = true;
+			wq.enqueue(next);
+		    }
+		}
+	    }
+	}
 
-       // We didn't find a common ancestor
-       sapLength = -1;
-       sapAncestor = -1;
-   }
+	// We didn't find a common ancestor
+	sapLength = -1;
+	sapAncestor = -1;
+    }
 
-   private void calculateSAP(Iterable<Integer> vIterable, Iterable<Integer> wIterable) {
-       int minLength = Integer.MAX_VALUE;
-       int minAncester = -1; 
-       for (int v : vIterable) {
-	   for (int w : wIterable) {
-	       calculateSAP(v, w);
-	       if (sapLength != -1 && sapLength < minLength) {
-		   minLength = sapLength;
-		   minAncester = sapAncestor;
-	       }
-	   }
-       }
 
-       if (minLength == Integer.MAX_VALUE) {
-	   sapLength = -1;
-	   sapAncestor = -1;
-       } else {
-	   sapLength = minLength;
-	   sapAncestor = minAncester;
-       }
-   }
+    // Calculate SAP across sets of v and w, looking for the shortest combination
+    private void calculateSAP(Iterable<Integer> vIterable, Iterable<Integer> wIterable) {
+	int minLength = Integer.MAX_VALUE;
+	int minAncester = -1; 
+	for (int v : vIterable) {
+	    for (int w : wIterable) {
+		calculateSAP(v, w);
+		if (sapLength != -1 && sapLength < minLength) {
+		    minLength = sapLength;
+		    minAncester = sapAncestor;
+		}
+	    }
+	}
 
-   // do unit testing of this class
-   public static void main(String[] args) {
-       In in = new In(args[0]);
-       Digraph G = new Digraph(in);
-       SAP sap = new SAP(G);
-       while (/*false &&*/ !StdIn.isEmpty()) {
-	   int v = StdIn.readInt();
-	   int w = StdIn.readInt();
-	   int length   = sap.length(v, w);
-	   int ancestor = sap.ancestor(v, w);
-	   StdOut.printf("length = %d, ancestor = %d\n", length, ancestor);
-       }
+	if (minLength == Integer.MAX_VALUE) {
+	    sapLength = -1;
+	    sapAncestor = -1;
+	} else {
+	    sapLength = minLength;
+	    sapAncestor = minAncester;
+	}
+    }
 
-       Integer v[] = {3, 9, 7, 1};
-       Integer w[] = {11, 12, 2, 6};
 
-       int length   = sap.length(Arrays.asList(v), Arrays.asList(w));
-       int ancestor = sap.ancestor(Arrays.asList(v), Arrays.asList(w));
-       StdOut.printf("length = %d, ancestor = %d\n", length, ancestor);
-   }
+    // Check that g contains vertex v
+    private static boolean isValidVertex(Digraph g, int v) {
+	return (0 <= v && v < g.V());
+    }
+
+
+    // do unit testing of this class
+    public static void main(String[] args) {
+	In in = new In(args[0]);
+	Digraph G = new Digraph(in);
+	SAP sap = new SAP(G);
+	while (/*false &&*/ !StdIn.isEmpty()) {
+	    int v = StdIn.readInt();
+	    int w = StdIn.readInt();
+	    int length   = sap.length(v, w);
+	    int ancestor = sap.ancestor(v, w);
+	    StdOut.printf("length = %d, ancestor = %d\n", length, ancestor);
+	}
+
+	Integer v[] = {3, 9, 7, 1};
+	Integer w[] = {11, 12, 2, 6};
+
+	int length   = sap.length(Arrays.asList(v), Arrays.asList(w));
+	int ancestor = sap.ancestor(Arrays.asList(v), Arrays.asList(w));
+	StdOut.printf("length = %d, ancestor = %d\n", length, ancestor);
+    }
 }
--- algorithms-course/wordnet/WordNet.java
+++ algorithms-course/wordnet/WordNet.java
@@ -2,94 +2,137 @@ import edu.princeton.cs.algs4.In;
 import edu.princeton.cs.algs4.Digraph;
 import edu.princeton.cs.algs4.StdOut;
 import edu.princeton.cs.algs4.StdIn;
+import edu.princeton.cs.algs4.DirectedCycle;
 
 import java.util.HashMap;
 import java.util.ArrayList;
+import java.lang.IllegalArgumentException;
 
+// This class represents the semantic "is-a" relationships between words using a DAG. An object
+// is initialized using a synset file (vertex information) and a hypernym file (edge information)
+// and then it allows for queries of the distance between two words through a common ancestor (aka
+// shortest ancestor path) and queries of closest common ancestor synset.
 public class WordNet {
     static final boolean DEBUG = false;
 
     private SAP sap;
-    private HashMap<String, ArrayList<Integer>> wordToIdListMap;
+    private HashMap<String, ArrayList<Integer>> wordToIdsMap;
     private HashMap<Integer, String> idToSynsetMap;
+
 
     // constructor takes the name of the two input files
     public WordNet(String synsets, String hypernyms) {
-	In synsetIn = new In(synsets);
-	String[] synsetLines = synsetIn.readAllLines();
+        In synsetIn = new In(synsets);
+        String[] synsetLines = synsetIn.readAllLines();
 
-	wordToIdListMap = new HashMap<String, ArrayList<Integer> >();
-	idToSynsetMap = new HashMap<Integer, String>();
-	for (String line : synsetLines) {
-	    String[] parts = line.split(",");
-	    int id = Integer.parseInt(parts[0]);
-	    idToSynsetMap.put(id, parts[1]);
-	    String[] synonyms = parts[1].split(" ");
-	    for (String synonym : synonyms) {
-		ArrayList<Integer> wordIdList = wordToIdListMap.get(synonym);
-		if (wordIdList == null) {
-		    wordIdList = new ArrayList<Integer>();
-		    wordToIdListMap.put(synonym, wordIdList);
-		}
-		wordIdList.add(id);
-	    }
-	}
+        initializeWordMaps(synsetLines);
 
-	if (DEBUG) {
-	    System.out.println(wordToIdListMap.toString());
-	}
+        In hypernymIn = new In(hypernyms);
+        String[] hypernymLines = hypernymIn.readAllLines();
+        Digraph graph = buildWordGraph(hypernymLines, synsetLines.length);
+        
+        if (DEBUG) {
+            System.out.println(graph.toString());
+        }
 
-	Digraph graph = new Digraph(synsetLines.length);
-	In hypernymIn = new In(hypernyms);
-	String[] hypernymLines = hypernymIn.readAllLines();
-	for (String hypernymStr : hypernymLines) {
-	    String[] idStrings = hypernymStr.split(",");
-	    int symsetId = Integer.parseInt(idStrings[0]);
-	    for (int i = 1; i < idStrings.length; i++) {
-		int hypernymId = Integer.parseInt(idStrings[i]);
-		graph.addEdge(symsetId, hypernymId);
-	    }
-	}
-
-	if (DEBUG) {
-	    System.out.println(graph.toString());
-	}
-
-	sap = new SAP(graph);
+	// Verify that the resulting graph is a rooted DAG
+        DirectedCycle directedCycle = new DirectedCycle(graph);
+        if (directedCycle.hasCycle() || !hasOneRoot(graph)) {
+            throw new IllegalArgumentException();
+        }
+        
+        sap = new SAP(graph);
     }
 
-   // returns all WordNet nouns
+    
+    // returns all WordNet nouns
+    public Iterable<String> nouns() {
+        return wordToIdsMap.keySet();
+    }
 
-   public Iterable<String> nouns() {
-       return wordToIdListMap.keySet();
-   }
 
-   // is the word a WordNet noun?
-   public boolean isNoun(String word) {
-       return wordToIdListMap.get(word) != null;
-   }
+    // is the word a WordNet noun?
+    public boolean isNoun(String word) {
+        return wordToIdsMap.get(word) != null;
+    }
 
-   // distance between nounA and nounB (defined below)
-   public int distance(String nounA, String nounB) {
-       return sap.length(wordToIdListMap.get(nounA), wordToIdListMap.get(nounB));
-   }
 
-   // a synset (second field of synsets.txt) that is the common ancestor of nounA and nounB
-   // in a shortest ancestral path (defined below)
-   public String sap(String nounA, String nounB) {
-       return idToSynsetMap.get(sap.ancestor(wordToIdListMap.get(nounA), wordToIdListMap.get(nounB)));
-   }
+    // distance between nounA and nounB (the length of the shortest ancestor path
+    // between the two words)
+    public int distance(String nounA, String nounB) {
+        return sap.length(wordToIdsMap.get(nounA), wordToIdsMap.get(nounB));
+    }
 
-   // do unit testing of this class
-   public static void main(String[] args) {
-       WordNet wordnet = new WordNet(args[0], args[1]);
-       while (!StdIn.isEmpty()) {
-	   String nounA = StdIn.readString();
-	   String nounB = StdIn.readString();
-	   int length   = wordnet.distance(nounA, nounB);
-	   String synset = wordnet.sap(nounA, nounB);
-	   StdOut.printf("length = %d, ancestor = %s\n", length, synset);
-       }
 
-   }
+    // a synset (second field of synsets.txt) that is the common ancestor of nounA and nounB
+    // in a shortest ancestral path
+    public String sap(String nounA, String nounB) {
+        return idToSynsetMap.get(sap.ancestor(wordToIdsMap.get(nounA), wordToIdsMap.get(nounB)));
+    }
+
+
+    private void initializeWordMaps(String[] synsetLines) {
+        wordToIdsMap = new HashMap<String, ArrayList<Integer> >();
+        idToSynsetMap = new HashMap<Integer, String>();
+        for (String line : synsetLines) {
+            String[] parts = line.split(",");
+            int id = Integer.parseInt(parts[0]);
+            idToSynsetMap.put(id, parts[1]);
+            String[] synonyms = parts[1].split(" ");
+            for (String synonym : synonyms) {
+                ArrayList<Integer> wordIdList = wordToIdsMap.get(synonym);
+                if (wordIdList == null) {
+                    wordIdList = new ArrayList<Integer>();
+                    wordToIdsMap.put(synonym, wordIdList);
+                }
+                wordIdList.add(id);
+            }
+        }
+    
+        if (DEBUG) {
+            System.out.println(wordToIdsMap.toString());
+        }
+    }
+
+
+    private Digraph buildWordGraph(String[] hypernymLines, int vertexCount) {
+        Digraph graph = new Digraph(vertexCount);
+        for (String hypernymStr : hypernymLines) {
+            String[] idStrings = hypernymStr.split(",");
+            int symsetId = Integer.parseInt(idStrings[0]);
+            for (int i = 1; i < idStrings.length; i++) {
+                int hypernymId = Integer.parseInt(idStrings[i]);
+                graph.addEdge(symsetId, hypernymId);
+            }
+        }
+
+        return graph;
+    }
+
+
+    // A "root" for this DAG has no outgoing edges. Return true if there is only
+    // one such node.
+    private boolean hasOneRoot(Digraph g) {
+	int rootsCount = 0;
+	for (int v = 0; v < g.V(); v++) {
+	    if (g.outdegree(v) == 0) {
+		rootsCount++;
+	    }
+	}
+
+	return rootsCount == 1;
+    }
+
+
+    // do unit testing of this class
+    public static void main(String[] args) {
+        WordNet wordnet = new WordNet(args[0], args[1]);
+        while (!StdIn.isEmpty()) {
+            String nounA = StdIn.readString();
+            String nounB = StdIn.readString();
+            int length   = wordnet.distance(nounA, nounB);
+            String synset = wordnet.sap(nounA, nounB);
+            StdOut.printf("length = %d, ancestor = %s\n", length, synset);
+        }
+    }
 }
