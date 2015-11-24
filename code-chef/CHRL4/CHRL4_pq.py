ef762817e81c52112b34d349549a898ee02eace8
Andrew Spencer <andrewpspencer@gmail.com>
2015-11-25 02:41:38 +0900

    Final version passes all tests

--- code-chef/CHRL4/CHRL4_pq.py
+++ code-chef/CHRL4/CHRL4_pq.py
@@ -5,22 +5,27 @@ import itertools
 import math
 
 def get_min_product_path(n,k,a):
+    s = [0]*n
     pq = []
+    s[0] = a[0]
     for i in range(1,min(k+1,n)):
         shortest_path = a[0]*a[i]
+        s[i] = shortest_path
         # shortest_path = a[0]*a[i]
-        heapq.heappush(pq,(shortest_path,i))
+        heapq.heappush(pq,(math.log(a[0])+math.log(a[i]),i))
 
-    for i in range(k+1,n-1):
+    for i in range(k+1,n):
         while i - pq[0][1] > k:
             heapq.heappop(pq)
-        shortest_path = a[i]*pq[0][0]
+        shortest_path = (a[i]*s[pq[0][1]]) % 1000000007
+        s[i] = shortest_path
+        heap_value = math.log(a[i]) + pq[0][0]
         if i - pq[0][1] == k:
-            heapq.heapreplace(pq,(shortest_path,i))
+            heapq.heapreplace(pq,(heap_value,i))
         else:
-            heapq.heappush(pq,(shortest_path,i))
+            heapq.heappush(pq,(heap_value,i))
 
-    return a[n-1]*pq[0][0]
+    return s[n-1]
 
 
 if __name__=="__main__":
