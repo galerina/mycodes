7cc3e731ece5f45804b2c87f71dd3c68792e054b
Andrew Spencer <andrewpspencer@gmail.com>
2015-11-29 23:19:33 +0900

    Checkpoint: passes correctness, need to improve speed

--- algorithms-course/boggle/BoggleSolver.java
+++ algorithms-course/boggle/BoggleSolver.java
@@ -1,6 +1,6 @@
 import edu.princeton.cs.algs4.In;
 import edu.princeton.cs.algs4.StdOut;
-import edu.princeton.cs.algs4.TST;
+// import edu.princeton.cs.algs4.TST;
 
 import java.util.TreeSet;
 import java.util.BitSet;
@@ -15,8 +15,6 @@ public class BoggleSolver {
         for (String s : dictionary) {
             this.dictionary.put(s, true);
         }
-
-        System.out.println(this.dictionary.keysWithPrefix("test"));
     }
 
     // Returns the set of all valid words in the given Boggle board, as an Iterable.
@@ -26,44 +24,56 @@ public class BoggleSolver {
         TreeSet<String> words = new TreeSet<String>();
         for (int i = 0; i < board.rows(); i++) {
             for (int j = 0; j < board.cols(); j++) {
-                getWordsHelper(board,new BitSet(),words,"",i,j);
+                getWordsHelper(board, new BitSet(), words, "", i, j);
             }
         }
 
         return words;
     }
 
+
+    private String getLetter(BoggleBoard board, int row, int col) {
+        char letter = board.getLetter(row, col);
+        if (letter == 'Q') {
+            return "QU";
+        } else {
+            return Character.toString(letter);
+        }
+    }
+
+
     private void getWordsHelper(BoggleBoard board, BitSet marked, TreeSet<String> words, String prefix, int row, int col) {
         marked.set(row * board.cols() + col);
-        String newPrefix = prefix + board.getLetter(row,col);
-        if (IsValidWord(newPrefix)) {
+        String newPrefix = prefix + getLetter(board, row, col);
+        if (newPrefix.length() >= 3 && dictionary.contains(newPrefix)) {
             words.add(newPrefix);
         }
 
-        if (IsValidPrefix(newPrefix)) {
+        if (newPrefix.length() <= 2 || dictionary.containsPrefix(newPrefix)) {
             for (int i = -1; i <= 1; i++) {
                 for (int j = -1; j <= 1; j++) {
                     int newRow = row + i;
                     int newCol = col + j;
-                    if (0 <= newRow && newRow < board.rows() && 0 <= newCol && newCol < board.cols() && !marked.get(newRow * board.cols() + newCol)) {
-                        getWordsHelper(board, (BitSet)marked.clone(), words, newPrefix, newRow, newCol);
+                    if (0 <= newRow && newRow < board.rows() && 
+                        0 <= newCol && newCol < board.cols() && !marked.get(newRow * board.cols() + newCol)) {
+                        getWordsHelper(board, (BitSet) marked.clone(), words, newPrefix, newRow, newCol);
                     }
                 }
             }
         }
     }
 
-    private boolean IsValidWord(String string) {
+    private boolean isValidWord(String string) {
         return (string.length() >= 3 && dictionary.contains(string));
-    }
-
-    private boolean IsValidPrefix(String string) {
-        return (dictionary.keysWithPrefix(string).iterator().hasNext());
     }
 
     // Returns the score of the given word if it is in the dictionary, zero otherwise.
     // (You can assume the word contains only the uppercase letters A through Z.)
     public int scoreOf(String word) {
+        if (!dictionary.contains(word)) {
+            return 0;
+        }
+
         if (word.length() <= 2) {
             return 0;
         } else if (word.length() <= 4) {
@@ -97,8 +107,8 @@ public class BoggleSolver {
         int M = 4;
         int N = 4;
         for (int i = 0; i < T; i++) {
-            BoggleBoard b = new BoggleBoard(M,N);
-            solver.getAllValidWords(board);
+            BoggleBoard b = new BoggleBoard(M, N);
+            Iterable<String> iter = solver.getAllValidWords(board);
         }
         long endTime = System.nanoTime();
         StdOut.printf("%g ms per solve, %dx%d board\n", (endTime - startTime) * 1.0 / (1000000*T), M, N);
